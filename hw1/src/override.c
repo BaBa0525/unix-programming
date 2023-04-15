07fbcdcbea3a14dc4b1e7385ea0e677b5299c548
BaBa0525 <hsunhsun0525@gmail.com>
2023-04-16 03:45:59 +0800

    ✨ Finish secure_open

--- hw1/src/override.c
+++ hw1/src/override.c
@@ -1,8 +1,11 @@
 #include "override.h"
 
+#include <errno.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "sandbox.h"
@@ -11,7 +14,30 @@
 int secure_open(const char *pathname, int flags, mode_t mode) {
     printf("secure_open() is called with pathname: %s, flags: %d, mode: %d\n",
            pathname, flags, mode);
-    return open(pathname, flags, mode);
+    char blacklist[PATH_MAX] = {}, real_pathname[PATH_MAX];
+    if (realpath(pathname, real_pathname) == NULL) {
+        printf("realpath() error: %s\n", strerror(errno));
+        return -1;
+    }
+    read_config(blacklist, "open");
+
+    char *blocked_path = strtok(blacklist, "\n");
+
+    while (blocked_path != NULL) {
+        char blocked_real_path[PATH_MAX] = {};
+        if (realpath(blocked_path, blocked_real_path) == NULL) {
+            printf("realpath() error: %s\n", strerror(errno));
+            return -1;
+        }
+        if (strcmp(real_pathname, blocked_real_path) == 0) {
+            printf("open() is blocked for %s\n", real_pathname);
+            errno = EACCES;
+            return -1;
+        }
+        blocked_path = strtok(NULL, "\n");
+    }
+
+    return open(real_pathname, flags, mode);
 }
 
 int secure_read(int fd, void *buf, size_t count) {
