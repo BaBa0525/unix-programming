86949f9ead67ea8aa1a87dd89bdf8e9c7ae50c54
BaBa0525 <hsunhsun0525@gmail.com>
2023-04-17 00:30:05 +0800

    ✨ Add secure_write and secure_connect

--- hw1/src/override.c
+++ hw1/src/override.c
@@ -1,5 +1,6 @@
 #include "override.h"
 
+#include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -99,26 +100,69 @@ ssize_t secure_read(int fd, void *buf, size_t count) {
 
 ssize_t secure_write(int fd, const void *buf, size_t count) {
     Logger("write(%d, %12p, %zu) = %zu\n", fd, buf, count, count);
+
+    char log_file_name[PATH_MAX] = {};
+    get_log_path(fd, WRITE, log_file_name);
+    FILE *fp = fopen(log_file_name, "a");
+    if (fp == NULL) {
+        perror("fopen()");
+        Logger("write(%d, %12p, %lu) = -1\n", fd, buf, count);
+        return -1;
+    }
+    fwrite(buf, 1, count, fp);
+    fclose(fp);
+
     return write(fd, buf, count);
 }
 
 int secure_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
-    printf(
-        "secure_connect() is called with sockfd: %d, addr: %p, addrlen: %d\n",
-        sockfd, addr, addrlen);
-    return connect(sockfd, addr, addrlen);
+    struct sockaddr_in *addr_in = (struct sockaddr_in *)addr;
+
+    char blacklist[512] = {};
+    read_config(blacklist, CONNECT);
+    char *blocked_addr = strtok(blacklist, "\n");
+    while (blocked_addr != NULL) {
+        char *blocked_hostname = strtok(blocked_addr, ":");
+        in_port_t blocked_port = strtoul(strtok(NULL, ":"), NULL, 10);
+        if (blocked_port != ntohs(addr_in->sin_port)) {
+            blocked_addr = strtok(NULL, "\n");
+            continue;
+        }
+        if (cmp_ip(addr_in->sin_addr, blocked_hostname)) {
+            errno = ECONNREFUSED;
+            Logger("connect(%d, \"%s\", %d) = -1\n", sockfd,
+                   inet_ntoa(addr_in->sin_addr), addrlen);
+            return -1;
+        }
+    }
+
+    int ret = connect(sockfd, addr, addrlen);
+    Logger("connect(%d, \"%s\", %d) = %d\n", sockfd,
+           inet_ntoa(addr_in->sin_addr), addrlen, ret);
+    return ret;
 }
 
 int secure_getaddrinfo(const char *node, const char *service,
                        const struct addrinfo *hints, struct addrinfo **res) {
-    printf(
-        "secure_getaddrinfo() is called with node: %s, service: %s, hints: %p, "
-        "res: %p\n",
-        node, service, hints, res);
-    return getaddrinfo(node, service, hints, res);
+    char blacklist[512] = {};
+    read_config(blacklist, GETADDRINFO);
+    char *blocked_node = strtok(blacklist, "\n");
+    while (blocked_node != NULL) {
+        if (strcmp(blocked_node, node) == 0) {
+            Logger("getaddrinfo(%s, %s, %p, %p) = %d\n", node, service, hints,
+                   res, EAI_NONAME);
+            return EAI_NONAME;
+        }
+        blocked_node = strtok(NULL, "\n");
+    }
+
+    int ret = getaddrinfo(node, service, hints, res);
+    Logger("getaddrinfo(\"%s\", \"%s\", %p, %p) = %d\n", node, service, hints,
+           res, ret);
+    return ret;
 }
 
 int secure_system(const char *command) {
-    printf("secure_system() is called with command: %s\n", command);
+    Logger("system(\"%s\")\n", command);
     return system(command);
 }
