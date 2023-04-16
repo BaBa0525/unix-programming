e93975fac0cabb4cac5607f380a46b508878c880
BaBa0525 <hsunhsun0525@gmail.com>
2023-04-17 00:31:07 +0800

    🔧 Modify config and test script

--- hw1/example.sh
+++ hw1/example.sh
@@ -3,7 +3,7 @@ case $1 in
 2) ./launcher ./sandbox.so config.txt cat /etc/hosts ;;
 3) ./launcher ./sandbox.so config.txt cat /etc/ssl/certs/Amazon_Root_CA_1.pem ;;
 5) ./launcher ./sandbox.so config.txt wget http://google.com -t 1 ;;
-6) ./launcher ./launcher ./sandbox.so config.txt wget https://www.nycu.edu.tw -t 1 ;;
+6) ./launcher ./sandbox.so config.txt wget https://www.nycu.edu.tw -t 1 ;;
 7) ./launcher ./sandbox.so config.txt wget http://www.google.com -q -t 1 ;;
 8) ./launcher ./sandbox.so config.txt python3 -c 'import os;os.system("wget http://www.google.com -q -t 1")' ;;
 *) echo "Usage: $0 {1|2|3|4|5|6|7|8}" ;;
